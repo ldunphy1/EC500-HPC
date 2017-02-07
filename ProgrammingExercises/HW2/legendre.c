4bee0ff701a7b2bd426bfc14e4b8659b317dbd20
Lindsey <ldunphy@bu.edu>
2017-02-06 22:06:14 -0500

    updated

--- ProgrammingExercises/HW2/legendre.c
+++ ProgrammingExercises/HW2/legendre.c
@@ -10,12 +10,17 @@ int getLegendreCoeff(double* A, int n)
 	double (*a)[n][n] = (double (*)[n][n]) A;
 	if((n<0) || (a==NULL))
 		return 0;
-	(*a)[0][0] = 1;
-	(*a)[1][0] = 0;
-	(*a)[1][1] = 1;
-	for(int j=n;j>=0;j--)
+	for(int i=0;i<=n;i++)
 	{
-		(*a)[n][j] = ((2*n-1) * (*a)[n-1][j-1] - (n-1) * (*a)[n-2][j])/n;		
+		(*a)[0][i] = 1;
+		(*a)[1][i] = i;
+	}
+	for(int i=0;i<=n;i++)
+	{
+		for(int j=0;j<=n;j++)
+		{
+			(*a)[i][j] = (2*i-1)/i * j * (*a)[i-1][j] - (i-1)i * (*a)[i-2][j];		
+		}
 	}
 	return 1;		
 }
