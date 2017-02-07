871c5f1c4985664c81c274449483e9a7eacd2071
Lindsey <ldunphy@bu.edu>
2017-02-06 22:22:15 -0500

    updated

--- ProgrammingExercises/HW2/legendre.c
+++ ProgrammingExercises/HW2/legendre.c
@@ -2,24 +2,25 @@
 #include <iomanip> 
 #include <cmath>
 #include <float.h>
+#define n 5
 
 using namespace std;
 
-int getLegendreCoeff(double* A, int n)
+int getLegendreCoeff(double[n][n] a, int n)
 {
-	double (*a)[n][n] = (double (*)[n][n]) A;
+	//double (*a)[n][n] = (double (*)[n][n]) A;
 	if((n<0) || (a==NULL))
 		return 0;
 	for(int i=0;i<=n;i++)
 	{
-		(*a)[0][i] = 1;
-		(*a)[1][i] = i;
+		a[0][i] = 1;
+		a[1][i] = i;
 	}
 	for(int i=0;i<=n;i++)
 	{
 		for(int j=0;j<=n;j++)
 		{
-			(*a)[i][j] = (2*i-1)/i * j * (*a)[i-1][j] - (i-1)i * (*a)[i-2][j];		
+			a[i][j] = (2*i-1)/i * j * a[i-1][j] - (i-1)i * a[i-2][j];		
 		}
 	}
 	return 1;		
@@ -31,7 +32,7 @@ int main()
 	cout<<"What order Legendre polynomial? "<<endl;
 	cin>>n;
 	double a[n+1][n+1];	
-	int x = getLegendreCoeff(*a,n);
+	int x = getLegendreCoeff(a,n);
 	cout<<a[n][0]<<" x^"<<n;
 	for(int i=n-1;i>=0;i--)
 	{
