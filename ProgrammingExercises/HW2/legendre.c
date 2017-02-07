fe91e83e0767db07a64acaa9aab140ccdd13ce71
ldunphy1 <ldunphy@bu.edu>
2017-02-06 23:47:40 -0500

    updated

--- ProgrammingExercises/HW2/legendre.c
+++ ProgrammingExercises/HW2/legendre.c
@@ -5,21 +5,27 @@
 
 using namespace std;
 
-int getLegendreCoeff(double* A, int n)
+int getLegendreCoeff(double* a, int n)
 {
-	double (*a)[n][n] = (double (*)[n][n]) A;
-	if((n<0) || (a==NULL))
-		return 0;
 	for(int i=0;i<=n;i++)
 	{
-		(*a)[0][i] = 1;
-		(*a)[1][i] = i;
-	}
+		for(int j=0;j<=n;j++)
+		{
+			a[i+j*(n+1)] = 0.0;
+		}
+	}	
+	a[0+0*(n+1)] = 1.0;
+	if(0<n)
+		a[1+1*(n+1)] = 1.0;
 	for(int i=2;i<=n;i++)
 	{
-		for(int j=n;j>=0;j--)
+		for(int j=0;j<=i-2;j++)
 		{
-			(*a)[i][j] = (2*i-1)/i * (*a)[i-1][j-1] - (i-1)/i * (*a)[i-2][j];		
+			a[i+j*(n+1)] = (double)(-i+1)*a[i-2+j*(n+1)]/(double)i;			
+		}
+		for(int j=1;j<=i;j++)
+		{
+			a[i+j*(n+1)] = a[i+j*(n+1)]+(double)(i+i-1)*a[i-1+(j-1)*(n+1)]/(double)i;
 		}
 	}
 	return 1;		
@@ -30,12 +36,12 @@ int main()
 	int n;
 	cout<<"What order Legendre polynomial? "<<endl;
 	cin>>n;
-	double a[n+1][n+1];	
-	int x = getLegendreCoeff((*a),n);
-	cout<<a[n][0]<<" x^"<<n;
+	double a[(n+1)*(n+1)];	
+	int x = getLegendreCoeff(a,n);
+	cout<<a[n+0*(n+1)]<<" x^"<<n;
 	for(int i=n-1;i>=0;i--)
 	{
-		cout<<" + "<<a[n][i]<<" x^"<<i;
+		cout<<" + "<<a[n+i*(n+1)]<<" x^"<<i;
 	}
 	cout<<endl;
 	
