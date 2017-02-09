4ac21e26f8bc3b4f574d1d753e24fc74a6c474d8
ldunphy1 <ldunphy@bu.edu>
2017-02-09 16:50:22 -0500

    finished legendre.c

--- ProgrammingExercises/HW2/legendre.c
+++ ProgrammingExercises/HW2/legendre.c
@@ -6,28 +6,15 @@
 using namespace std;
 
 int getLegendreCoeff(double* a, int n)
-{
-	for(int i=0;i<=n;i++)
-	{
-		for(int j=0;j<=n;j++)
-		{
-			a[i+j*(n+1)] = 0.0;
-		}
-	}	
-	a[0+0*(n+1)] = 1.0;
-	if(0<n)
-		a[1+1*(n+1)] = 1.0;
-	for(int i=2;i<=n;i++)
-	{
-		for(int j=0;j<=i-2;j++)
-		{
-			a[i+j*(n+1)] = (double)(-i+1)*a[i-2+j*(n+1)]/(double)i;			
-		}
-		for(int j=1;j<=i;j++)
-		{
-			a[i+j*(n+1)] = a[i+j*(n+1)]+(double)(i+i-1)*a[i-1+(j-1)*(n+1)]/(double)i;
-		}
-	}
+{		
+	a[0+0*(n+1)] = 1.0; //set P0
+	a[1+1*(n+1)] = 1.0; //set P1
+    for(int pn=2; pn<n+1;pn++)
+    { 
+    	a[0 +pn*(n+1)] = -((pn-1)*a[0 + (pn-2)*(n+1)])/(double) pn;
+        for(int i =1;i < pn+1;i++)
+    		a[i+pn*(n+1)] = ((2*pn-1)*a[i-1 +(pn-1)*(n+1)] - (pn-1)*a[i + (pn-2)*(n+1)])/(double) pn;
+    }
 	return 1;		
 }
 
@@ -36,12 +23,16 @@ int main()
 	int n;
 	cout<<"What order Legendre polynomial? "<<endl;
 	cin>>n;
-	double a[(n+1)*(n+1)];	
+	double* a = new double((n+1)*(n+1));	
+	for(int i=0;i<(n+1)*(n+1);i++)
+	{
+		a[i] =0.0;
+	}
 	int x = getLegendreCoeff(a,n);
-	cout<<a[n+0*(n+1)]<<" x^"<<n;
+	cout<<a[n+n*(n+1)]<<" x^"<<n;
 	for(int i=n-1;i>=0;i--)
 	{
-		cout<<" + "<<a[n+i*(n+1)]<<" x^"<<i;
+		cout<<" + "<<a[i +  n*(n+1)]<<" x^"<<i;
 	}
 	cout<<endl;
 	
