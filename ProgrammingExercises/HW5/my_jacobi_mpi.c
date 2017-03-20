b7dceb4dc1f7ccdb170a56d26deaf5f3db0faae2
Lindsey <ldunphy@bu.edu>
2017-03-20 15:28:05 -0400

    debugging

--- ProgrammingExercises/HW5/my_jacobi_mpi.c
+++ ProgrammingExercises/HW5/my_jacobi_mpi.c
@@ -30,7 +30,7 @@ int main(int argc, char** argv)
 {
 	for(int N=16; N<=16384; N*=4)
 	{
-		printf("%d\n", N);	
+		printf("begin %d\n", N);	
 	   int i,totiter;
 	   int done = 0;
 	   double *x, *xtmp, *b; 
@@ -48,10 +48,10 @@ int main(int argc, char** argv)
 	   
 	   // Figure out my local size. The last rank gets the leftover. 
 	   local_size = N/world_size;
-	   printf("%d\n", N);
+	   printf("local size set %d\n", N);
 	   
 	   if (my_rank == (world_size-1)) { local_size += (N % world_size) ; }
-     	printf("%d\n", N);
+     	printf("checked my rank=world size %d\n", N);
 
 	   //printf("I am rank %d of %d and I have a local size %d.\n", my_rank, world_size, local_size); 
 	   x = (double*)malloc(local_size*sizeof(double));
@@ -63,10 +63,10 @@ int main(int argc, char** argv)
 	   // b[N/2] = 1.0;
 	   // The source only lives on a particular rank!
 	   int source_rank = (N/2)/(N/world_size);
-		printf("%d\n", N);
+		printf("source rank set %d\n", N);
 	   //printf("The source at %d goes on rank %d.\n", N/2, source_rank);
 	   if (my_rank == source_rank) { b[N/2 - source_rank*(N/world_size)] = 1.0; }
-	   printf("%d\n", N);
+	   printf("cheecked my rank=source rank%d\n", N);
 	   
 	   //Get magnitude of rhs
 	   bmag = magnitude(b, local_size);
@@ -90,7 +90,7 @@ int main(int argc, char** argv)
 	      
 	      if (resmag/bmag < RESID) { done = 1; }
 	   }
-	   printf("%d\n", N);
+	   printf("finished interations %d\n", N);
 	   free(x); free(xtmp); free(b);
 
 	   // Clean up
